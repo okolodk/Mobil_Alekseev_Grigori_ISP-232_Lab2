c47554cd9ec7e637de141b2a456419bd3a23e034
Nadia Rykhlyuk <nadigar1506@mail.ru>
2025-09-14 14:58:50 +0300

    Задание 2

---
+++ src/Laba.kt
@@ -0,0 +1,20 @@
+fun main() {
+    //Задание 1
+//    val level = readln().toInt()
+//    if (level in 1 until  10) {
+//        println("Новичок. ВЫ получаете бонус +10 HP.")
+//    } else if (level in 11 until 50 ) {
+//        println("Опытный. ВЫ получаете +30 HP.")
+//    } else if( level >= 50) {
+//        println("Легенда. ВЫ получаете +100 HP.")
+//    }
+//Задание 2
+//    val age = readln().toInt()
+//    if (age < 12) {
+//        println("Ребёнок")
+//    } else if (age in 12..17) {
+//        println("Подросток")
+//    } else if (age > 17) {
+//        println("Взрослый")
+//    }
+}
