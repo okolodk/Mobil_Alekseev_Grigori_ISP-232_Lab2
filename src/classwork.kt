63833c7a3bfd9b1c34cc8ac63cfccad3d6684644
Nadia Rykhlyuk <nadigar1506@mail.ru>
2025-09-15 11:49:57 +0300

    Магический щит (защита от урона)

---
+++ src/classwork.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //Задание: Магический щит (защита от урона)
+//    print("Введите урон: ")
+//    var demag = readln().toInt()
+//    print("Щит активен (Да или Нет): ")
+//    var shield = readln()
+//
+//    if (shield == "Да") {
+//        println("Полученный урон ${demag / 2}")
+//    } else {
+//        println("Полученный урон ${demag}")
+//    }
+}
